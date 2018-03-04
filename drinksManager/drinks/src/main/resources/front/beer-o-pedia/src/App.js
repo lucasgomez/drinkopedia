a6eedab1c195e3eeb5a0bdc60d8b549a1bd65292
Lucas Gomez <lucas.gomez.ch@gmail.com>
2018-03-04 17:01:17 +0100

    Tabs

--- drinksManager/drinks/src/main/resources/front/beer-o-pedia/src/App.js
+++ drinksManager/drinks/src/main/resources/front/beer-o-pedia/src/App.js
@@ -2,18 +2,74 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 //import './App.css';
 //import 'bootstrap/less/bootstrap.less'
-import {Bootstrap, Grid, Row, Col} from 'react-bootstrap';
+import {Bootstrap, Row, Col, ButtonGroup, Button, DropdownButton, MenuItem, Tabs, Tab, Jumbotron} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
 class App extends Component {
   render() {
+	  return (
+		      <div className="game">
+		        <div className="game-board">
+		          <Welcome />
+		        </div>
+		      </div>
+		    );
+  }
+}
+
+class Welcome extends Component {
+	render() {
+		return (
+	    		<container>
+    				<Jumbotron>
+						<h1>{ 'Beer-O-Pedia' }</h1>
+						<p>{ 'Tu boiras moins bête' }</p>
+					</Jumbotron>
+
+					<WelcomeTabs/>
+				</container>
+	    );
+	}
+}
+
+class WelcomeTabs extends Component {
+  constructor(props) {
+	    super();
+	    this.state = {
+      activeTab: props.activeTab || 1
+    };
+    
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  
+  render() {
     return (
-		<div className="page-header">
-			<h1>Beer-O-Pedia<br/>
-			<small>Tu boiras moins bete</small></h1>
-		</div>
+		<Tabs>
+		<Tab eventKey="1" title={ 'Les cartes de Bière' }>
+			<p>{ 'Tu trouveras ci-dessous les listes de nos Bières classées :' }</p>
+			<ButtonGroup vertical>
+				<Button>{ 'Par nom' }</Button>
+				<Button>{ 'Par bar' }</Button>
+				<DropdownButton title="Par couleur" id="bg-vertical-dropdown-1">
+					<MenuItem eventKey="1">{ 'Blanche' }</MenuItem>
+					<MenuItem eventKey="2">{ 'Blonde' }</MenuItem>
+					<MenuItem eventKey="3">{ 'Rousse' }</MenuItem>
+					<MenuItem eventKey="4">{ 'Brune / Noire' }</MenuItem>
+				</DropdownButton>
+			</ButtonGroup>
+		</Tab>
+		<Tab eventKey="2" title={ 'En savoir plus' }>
+			<p>{ 'Qui n\'a jamais rêvé de tout savoir sur la Bière? Désormais, c\'est possible!' }</p>
+			</Tab>
+		</Tabs>
     );
+  }
+  
+  handleSelect(selectedTab) {
+    this.setState({
+      activeTab: selectedTab
+    });
   }
 }
 
