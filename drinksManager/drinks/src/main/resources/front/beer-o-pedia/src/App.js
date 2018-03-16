4c2a58cc84b7d184a546884992ff76d904f10095
Lucas Gomez <lucas.gomez.ch@gmail.com>
2018-03-16 19:31:50 +0100

    All lists visible

--- drinksManager/drinks/src/main/resources/front/beer-o-pedia/src/App.js
+++ drinksManager/drinks/src/main/resources/front/beer-o-pedia/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-//import './App.css';
-//import 'bootstrap/less/bootstrap.less'
 import {Bootstrap, Row, Col, ButtonGroup, Button, DropdownButton, MenuItem, Tabs, Tab, Jumbotron} from 'react-bootstrap';
+import ListButton from './ListButton';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
@@ -38,63 +36,9 @@ class WelcomeTabs extends Component {
 	    super();
 	    this.state = {
 	      activeTab: props.activeTab || 1,
-	      colors: []
 	    };
     
     this.handleSelect = this.handleSelect.bind(this);
-  }
-  
-  componentDidMount() {
-	  let base64 = require('base-64');
-	  let url = 'http://localhost:8081/drinkopedia/beers/colors/list';
-	  let username = 'belzeboss';
-	  let password = '666';
-	  var request = require('superagent');
-	  
-	    var self = this;
-
-//      request
-//	    .get('http://localhost:8081/drinkopedia/api/beers/colors/list')
-//	    .withCredentials()
-//	    .auth('belzeboss', '666', {type:'auto'})
-//	    .then(response => console.log(response));
-//	    .then(function(res) {
-//		      // res.body, res.headers, res.status
-//		   })
-//		   .catch(function(err) {
-//		      // err.message, err.response
-//		   });
-//		  .then(data => {
-//			  let colorButtons = data.results.map((color) => {
-//				  return (
-//						  <MenuItem eventKey="1">{ color.name }</MenuItem>
-//				  )
-//			  })
-//			  debugger;
-//			  this.setState({colors: colorButtons});
-//		  });
-      
-//		let headers = new Headers();
-//		headers.append('Authorization', 'Basic' + base64.encode(username + ":" + password));
-		
-		fetch(url, {mode: 'cors'})
-//			{
-//				method:'GET',
-//				headers: headers,
-//	         })
-		.then(response => response.json())
-//		.then(json => console.log(json));
-	    
-//  	   .then(results => results.json())
-  	   .then(data => {
-  		  let colorButtons = data.entity.map((color) => {
-  			  return (
-  					  <MenuItem eventKey="1">{ color.name }</MenuItem>
-  			  )
-  		  })
-  		  this.setState({colors: colorButtons});
-  	  });
-	    
   }
   
   render() {
@@ -106,7 +50,16 @@ class WelcomeTabs extends Component {
 					<Button>{ 'Par nom' }</Button>
 					<Button>{ 'Par bar' }</Button>
 					<DropdownButton title="Par couleur" id="bg-vertical-dropdown-1">
-						{this.state.colors}
+						<ListButton listName="colors"/>
+					</DropdownButton>
+					<DropdownButton title="Par style" id="bg-vertical-dropdown-1">
+						<ListButton listName="styles"/>
+					</DropdownButton>
+					<DropdownButton title="Par producteur" id="bg-vertical-dropdown-1">
+						<ListButton listName="producers"/>
+					</DropdownButton>
+					<DropdownButton title="Par origine" id="bg-vertical-dropdown-1">
+						<ListButton listName="places"/>
 					</DropdownButton>
 				</ButtonGroup>
 			</Tab>
