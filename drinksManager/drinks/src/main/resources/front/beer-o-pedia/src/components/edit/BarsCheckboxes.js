7160bf859f785d80d27d9eac0887fd3b2152d799
Lucas Gomez <lucas.gomez.ch@gmail.com>
2019-04-09 16:13:45 +0200

    Bars checkboxes from fetched list

---
+++ drinksManager/drinks/src/main/resources/front/beer-o-pedia/src/components/edit/BarsCheckboxes.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { API_ROOT } from '../../data/apiConfig';
+import { ReactstrapRadio } from "reactstrap-formik";
+import { Field } from 'formik';
+
+class BarsCheckboxes extends Component {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      items: [],
+      isLoading: false
+    };
+  }
+
+  componentDidMount() {
+    debugger;
+    this.setState({
+      isLoading: true
+    });
+    let baseUrl = `${API_ROOT}/public/lists/bars`;
+
+    fetch(baseUrl)
+      .then(response => response.json())
+      .then(items =>
+        this.setState({
+          items: items,
+          isLoading: false
+        })
+      );
+  }
+
+  renderCheckbox = (name, value, label) => {
+    return (
+      <Field name={name}>
+        {({ field, form }) => (
+          <label>
+            <input
+              type="checkbox"
+              checked={field.value.includes(value)}
+              onChange={() => {
+                if (field.value.includes(value)) {
+                  const nextValue = field.value.filter(
+                    value => value !== value
+                  );
+                  form.setFieldValue(name, nextValue);
+                } else {
+                  const nextValue = field.value.concat(value);
+                  form.setFieldValue(name, nextValue);
+                }
+              }}
+            />
+            {label}
+          </label>
+        )}
+      </Field>
+    );
+  }
+
+  render() {
+    const {
+      items,
+      isLoading
+    } = this.state;
+    // const name = this.props.name;
+    // const label = this.props.label;
+    debugger;
+    if (isLoading || !items || items.length == 0) {
+      return (
+        <div>Loading...</div>
+      );
+    }
+
+    return (
+      <div>
+      {items.map((item: any) =>
+        this.renderCheckbox("bottleBars", item.id, item.name)
+      )}
+      </div>
+    );
+  }
+}
+
+export default BarsCheckboxes;
