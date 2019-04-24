cff0864e0126a8646444259beac1a24cb51dd6a9
Lucas Gomez <lucas.gomez.ch@gmail.com>
2019-04-24 10:43:16 +0200

    Reverted from port 8081 to 8080 and removed context path */drinkopedia

--- drinksManager/drinks/src/main/resources/front/beer-o-pedia/src/data/apiConfig.js
+++ drinksManager/drinks/src/main/resources/front/beer-o-pedia/src/data/apiConfig.js
@@ -4,7 +4,7 @@ const hostname = window && window.location && window.location.hostname;
 // if(hostname === 'http://soif.fetedelabiere.ch/') {
   // backendHost = 'http://drinks.elveteek.ch';
 // } else {
-  backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:8081/drinkopedia';
+  backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:8080';
 // }
 
 export const API_ROOT = `${backendHost}`;
