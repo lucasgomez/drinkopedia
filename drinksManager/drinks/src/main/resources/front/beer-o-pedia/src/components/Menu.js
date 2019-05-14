92558ebc46e244c09394ff1369416d37780748e4
Lucas Gomez <lucas.gomez.ch@gmail.com>
2019-05-14 18:15:01 +0200

    Search bar (not yet routed)

--- drinksManager/drinks/src/main/resources/front/beer-o-pedia/src/components/Menu.js
+++ drinksManager/drinks/src/main/resources/front/beer-o-pedia/src/components/Menu.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import {ButtonGroup, Button} from 'react-bootstrap';
 import ButtonsList from './ButtonsList';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import SearchField from "react-search-field";
 
 class Menu extends Component {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      fireRedirect: false,
+      redirectUrl: null,
+    };
+  }
+
+  redirectToSearch = (value) => {
+    debugger;
+    this.setState({
+      fireRedirect: true,
+      redirectUrl: '/search/'+encodeURIComponent(value)
+    });
+  }
+
   render() {
+
+    const {
+      fireRedirect,
+      redirectUrl,
+    } = this.state;
+
     return (
-      <div className="container">
+      <div className="container-fluid">
+        {fireRedirect && (
+          <Redirect to={redirectUrl}/>
+        )}
+
 				<ButtonGroup>
 					<Button>
             <Link to={'/list/'}>
@@ -19,6 +47,10 @@ class Menu extends Component {
 					<ButtonsList listName="producers" title="Par producteur"/>
 					<ButtonsList listName="origins" title="Par origine"/>
 				</ButtonGroup>
+        <SearchField
+          placeholder="Rechercher..."
+          onSearchClick={this.redirectToSearch}
+        />
       </div>
     );
   }
